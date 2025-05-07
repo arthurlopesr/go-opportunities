75f49b7f6c526f5e673f2d9832ef9825d802bf75
Arthur Lopes <arthurlopr12@gmail.com>
2025-05-07 15:31:35 -0300

    feat: update handler structure to separate subpackage

--- router/routes.go
+++ router/routes.go
@@ -2,37 +2,17 @@ package router
 
 import (
 	"github.com/arthurlopesr/go-opportunities/constant"
+	"github.com/arthurlopesr/go-opportunities/handler"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group(constant.APIBasePath)
 	{
-		v1.GET(constant.APIOpeningPath, func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Hello, World!",
-			})
-		})
-		v1.POST(constant.APIOpeningPath, func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": " post Hello, World!",
-			})
-		})
-		v1.DELETE(constant.APIOpeningPath, func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Delete Hello, World!",
-			})
-		})
-		v1.PUT(constant.APIOpeningPath, func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "PUT Hello, World!",
-			})
-		})
-		v1.GET(constant.APIOpeningsPath, func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "find all Hello, World!",
-			})
-		})
+		v1.GET(constant.APIOpeningPath, handler.FindOpeningHandler)
+		v1.POST(constant.APIOpeningPath, handler.CreateOpeningHandler)
+		v1.DELETE(constant.APIOpeningPath, handler.DeleteOpeningHandler)
+		v1.PUT(constant.APIOpeningPath, handler.UpdateOpeningHandler)
+		v1.GET(constant.APIOpeningsPath, handler.FindAllOpeningHandler)
 	}
 }
