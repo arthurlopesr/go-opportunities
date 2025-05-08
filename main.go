e92b85fed51ab3d1b14b37ca3f62cd8413cdfd47
Arthur Lopes <arthurlopr12@gmail.com>
2025-05-08 14:55:17 -0300

    feat: add log config

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/arthurlopesr/go-opportunities/router"
+import (
+	"github.com/arthurlopesr/go-opportunities/config"
+	"github.com/arthurlopesr/go-opportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.ErrorFormated("Config initialization failed: %v", err)
+		return
+	}
 	router.Initialize()
 }
