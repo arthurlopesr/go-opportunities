5359dae0e8986d83f6878a79f2cf50aff9f1211a
Arthur Lopes <arthurlopr12@gmail.com>
2025-05-10 10:04:31 -0300

    feat: add findAll function

--- handler/findAllOpeningsHandler.go
+++ handler/findAllOpeningsHandler.go
@@ -1,12 +1,17 @@
 package handler
 
 import (
+	"github.com/arthurlopesr/go-opportunities/dto"
+	"github.com/arthurlopesr/go-opportunities/schemas"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func FindAllOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Hello, World!",
-	})
+	var openings []schemas.Opening
+	if err := db.Find(&openings).Error; err != nil {
+		dto.SendErrorResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+	dto.SendSuccessResponse(ctx, "find-all-openings", http.StatusOK, openings)
 }
